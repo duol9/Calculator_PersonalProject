c40eaa32fb74fbe168dd96b3574f4dbdcc368cf1
duol9 <lhyy0195@gmail.com>
2024-11-18 13:24:25 +0900

    feat : 계산기 프로그램 종료하기 기능 추가
    이전 코드는 'exit' 입력 시 프로그램이 아예 종료되어 '기록 삭제하기'를 함께 쓸 수 없었음.
    "3. 종료하기" 옵션을 추가해서 '계산 끝마치기'와 '계산기 프로그램 종료'로 구분하여 계산 후 'exit'를 입력해도 프로그램이 바로 종료되지 않고 옵션 선택을 불러와 최근 기록도 삭제할 수 있도록 개선

--- src/Calculator_Lv2/App.java
+++ src/Calculator_Lv2/App.java
@@ -12,56 +12,60 @@ public class App {
         int result;
         int cal_op;
 
-        System.out.println("<계산기를 실행합니다.>");
-        System.out.println("1. 계산기 ");
-        System.out.println("2. 최근 계산 기록 삭제하기");
+        while(true) {
+            System.out.println("<계산기를 실행합니다.>");
+            System.out.println("1. 계산기 ");
+            System.out.println("2. 최근 계산 기록 삭제하기");
+            System.out.println("3. 계산기 종료하기");
+            System.out.print("옵션을 입력하세요. : ");
+            cal_op = sc.nextInt();
+            sc.nextLine();
 
-        System.out.println("사용하려는 기능의 번호를 입력하세요. : ");
-        cal_op = sc.nextInt();
-        sc.nextLine();
+            if (cal_op == 1) {
+                do {
+                    System.out.print("첫 번째 숫자를 입력하세요 : ");
+                    num1 = sc.nextInt();
+                    System.out.print("두 번째 숫자를 입력하세요 : ");
+                    num2 = sc.nextInt();
 
-        if (cal_op == 1) {
-            do {
-                System.out.print("첫 번째 숫자를 입력하세요 : ");
-                num1 = sc.nextInt();
-                System.out.print("두 번째 숫자를 입력하세요 : ");
-                num2 = sc.nextInt();
+                    // nextInt로 인해 생긴 버퍼 비우기
+                    sc.nextLine();
 
-                // nextInt로 인해 생긴 버퍼 비우기
-                sc.nextLine();
+                    if (num1 >= 0 && num2 >= 0) { // num1, num2가 양수라면
 
-                if (num1 >= 0 && num2 >= 0) { // num1, num2가 양수라면
+                        System.out.print("사칙연산 기호(➕,➖,✖️,➗)를 입력하세요. : ");
+                        String ops = sc.nextLine();
+                        char op = ops.charAt(0);
 
-                    System.out.print("사칙연산 기호(➕,➖,✖️,➗)를 입력하세요. : ");
-                    String ops = sc.nextLine();
-                    char op = ops.charAt(0);
-
-                    // 나눗셈 연산 시 분모 자리에 0이 올 경우, 계산을 계속 할 지 결정
-                    if (num2 == 0 && op == '/') {
-                        System.out.println("분모에 0이 올 수 없습니다. ");
+                        // 나눗셈 연산 시 분모 자리에 0이 올 경우, 계산을 계속 할 지 결정
+                        if (num2 == 0 && op == '/') {
+                            System.out.println("분모에 0이 올 수 없습니다. ");
+                        } else {
+                            result = calculator.calculate(num1, num2, op);
+                            System.out.println("계산 결과 : " + result);
+                        }
                     } else {
-                        result = calculator.calculate(num1, num2, op);
-                        System.out.println("계산 결과 : " + result);
+                        System.out.println("음수를 입력하셨습니다.");
                     }
-                } else {
-                    System.out.println("음수를 입력하셨습니다.");
+
+                    System.out.println("계산기를 계속 사용 하시겠습니까? (exit 입력 시 종료) ");
+                    exit_str = sc.nextLine();
+
+                } while(!exit_str.equals("exit"));
+
+            } else if (cal_op == 2) {
+                calculator.removeResult();
+                System.out.println("사용 기록");
+
+                int i = 1;
+
+                for (int res : calculator.getResultQueue()) {
+                    System.out.println(++i + ". " + res);
                 }
-
-                System.out.println("계산기를 계속 사용 하시겠습니까? (exit 입력 시 종료) ");
-                exit_str = sc.nextLine();
-
-            } while(!exit_str.equals("exit"));
-
-        } else if (cal_op == 2) {
-            calculator.removeResult();
-            System.out.println("사용 기록");
-
-            int i = 1;
-
-            for (int res : calculator.getResultQueue()) {
-                System.out.println(++i + ". " + res);
+            } else if (cal_op == 3) {
+                //3. 종료하기 선택 시 반복문을 빠져나와 프로그램 종료
+                break;
             }
         }
-
     }
 }
