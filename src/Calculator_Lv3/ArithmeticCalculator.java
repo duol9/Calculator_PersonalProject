61f4a12f34142dfaabc7277a68c2155031366739
duol9 <lhyy0195@gmail.com>
2024-11-19 20:11:30 +0900

    feat : ArithmeticCalculator 클래스 생성
    연산 결과를 담을 Queue 타입의 resultQue 선언
    ArithmeticCalculator 생성자 구현

--- src/Calculator_Lv3/ArithmeticCalculator.java
+++ src/Calculator_Lv3/ArithmeticCalculator.java
@@ -1,5 +1,18 @@
 package Calculator_Lv3;
 
-public class ArithmeticCalculator {
+import java.util.LinkedList;
+import java.util.Queue;
+
+// 제네릭 / Number클래스를 상속받는 이유는 intvalue, doublevlaue 메소를 사용해 객체를 정수/실수 타입으로 바꾸기 위해서
+public class ArithmeticCalculator <T>  {
+    // 속성
+    private Queue<T> resultQue;
+
+    //생성자
+    public ArithmeticCalculator() {
+        this.resultQue = new LinkedList<>();
+    }
+
+    // 기능(메소드)
 
 }
