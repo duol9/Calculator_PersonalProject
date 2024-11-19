d5d561681274f22c88c11363523d9de59df43e68
duol9 <lhyy0195@gmail.com>
2024-11-20 01:23:26 +0900

    feat : symbol 검색 기능 메소드 추가
    searchBySymbol() 메소드를 통해 문자열 심볼로 OperatorType 검색 기능 구현
    - 해당 메소드를 통해 심볼을 찾고, 외부로 반환 가능
    - 질못된 심볼 입력 시 IllegalArgumentException 발생

--- src/Calculator_Lv3/OperatorType.java
+++ src/Calculator_Lv3/OperatorType.java
@@ -10,7 +10,7 @@ public enum OperatorType {
 
     private final String symbol;
 
-    // enum 생성자
+    // enum 생성자.  private이 기본임
     OperatorType(String symbol) {
         this.symbol = symbol;
     }
@@ -18,5 +18,13 @@ public enum OperatorType {
     //enum 생성자는 private임 -> 외부와 직접적으로 교류를 하지 못하므로 getter 메소드 구현
     public String getSymbol(){
         return symbol;
+    }
+
+    public OperatorType searchBySymbol (String symbol){
+        for (OperatorType op : values()) { // values()를 사용해 OperatorType enum에 들어있는 상수들을 순차적으로 op에 반환
+            if (op.symbol.equals(symbol)) return op; // 매개변수로 받은 symbol이 OperatorType enum 상수와 일치한지 확인 후, 일치하면 해당 op(symbol) 반환
+        }
+        // enum상수에 없는 연산 기호라면 예외처리 후 종료
+        throw new IllegalArgumentException("지원하지 않는 연산 기호 입니다.");
     }
 }
