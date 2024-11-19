a411c392de9402c832f2421cdfde2a42c0f1e8bc
duol9 <lhyy0195@gmail.com>
2024-11-19 20:12:49 +0900

    comment : OperatorType 주석 추가

--- src/Calculator_Lv3/OperatorType.java
+++ src/Calculator_Lv3/OperatorType.java
@@ -1,6 +1,8 @@
 package Calculator_Lv3;
 
+
 public enum OperatorType {
+    // 각 연산자(+, -, *, /)에 대한 심볼 정의
     ADDITION("+"),
     SUBTRACTION("-"),
     MULTIPLICATION("*"),
@@ -8,10 +10,12 @@ public enum OperatorType {
 
     private final String symbol;
 
+    // enum 생성자
     OperatorType(String symbol) {
         this.symbol = symbol;
     }
 
+    //enum 생성자는 private임 -> 외부와 직접적으로 교류를 하지 못하므로 getter 메소드 구현
     public String getSymbol(){
         return symbol;
     }
