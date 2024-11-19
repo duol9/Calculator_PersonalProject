8f880adf7141419b1df69a0740d6fbeaf6e83524
duol9 <lhyy0195@gmail.com>
2024-11-20 02:35:16 +0900

    feat : 피연산자 타입 확장
    ArithmeticCalculator 객체 타입 설정
    - Number 타입의 Queue가 생성될 수 있도록 개선
    그 외 변수 타입 Number 타입으로 반영
    양수 범위 확인하는 조건문 개선
    - int보다 넓은 범위인 double로 형변환을 해 0을 포함한 양수를 가진 피연산자 인지 확인

--- src/Calculator_Lv3/App.java
+++ src/Calculator_Lv3/App.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator(); // arithmeticCalculator 인스턴스 생성
-        OperatorType operatorType;
+        ArithmeticCalculator<Number> arithmeticCalculator = new ArithmeticCalculator<>(); // arithmeticCalculator 인스턴스 생성
         Scanner sc = new Scanner(System.in);
 
         String exit_str = "";  // 연산 종료 입력 문자열
-        int num1, num2;        // 피연산자
-        int result;            // 연산 결과
-        int cal_op;            // 사칙연산 기호
+        Number num1, num2;        // 피연산자
+        Number result;            // 연산 결과
+        String s_num1, s_num2;    // 숫자 타입 결정하기 전 입력받을 문자열
+        int cal_op;            // 계산기 옵션 기호
 
         while(true) {
             // 계산기 옵션
@@ -38,14 +38,14 @@ public class App {
                     // nextInt로 인해 생긴 버퍼 비우기
                     sc.nextLine();
 
-                    if (num1 >= 0 && num2 >= 0) { // num1, num2가 양수라면 연산 수행
+                    if (num1.doubleValue() >= 0 && num2.doubleValue() >= 0) { // num1, num2가 양수라면 연산 수행
 
                         System.out.print("사칙연산 기호(➕,➖,✖️,➗)를 입력하세요. : ");
                         String ops = sc.nextLine(); // char 타입으로 입력을 받을 수 없어서 String타입으로 연산 기호 입력을 받고
 
                         // Calculator 클래스 calculate 메소드에 양수 num1, num2와 연산기호 op를 매개변수로 넘겨 연산을 수행
                         // 수행된 연산 결과를 result 변수로 반환
-                        result = arithmeticCalculator.calculate(num1, num2, OperatorType.searchBySymbol(ops));
+                        result = (Number) arithmeticCalculator.calculate(num1, num2, OperatorType.searchBySymbol(ops));
                         System.out.println("계산 결과 : " + result);
 
                     } else {
@@ -59,13 +59,13 @@ public class App {
                 } while(!exit_str.equals("exit"));  // exit_str에 exit가 들어있으면 do - while문 종료, 아니라면 계속 진행
 
             } else if (cal_op == 2) {       // cal_op == 2라면 가장 먼저 저장된 결과를
-                calculator.removeResult(); // 삭제하는 removeResult()를 Calculator 클래스에서 호출 후, 가장 먼저 저장된 기록 삭제
+                arithmeticCalculator.removeResult(); // 삭제하는 removeResult()를 Calculator 클래스에서 호출 후, 가장 먼저 저장된 기록 삭제
                 System.out.println("사용 기록");
 
                 int i = 1;
                 // Caclculator클래스 getResultQueue 메소드로 결과가 저장된 queue를 참조할 수 있도록 함
                 // queue에 저장된 값들을 res에 대입 후, 순서대로 하나씩 출력
-                for (int res : calculator.getResultQueue()) {
+                for (Number res : arithmeticCalculator.getResultQue()) {
                     System.out.println(i++ + ". " + res);  // 해당 출력문 실행이 끝나면 i가 1씩 증가하도록 후위 증가 연산자 사용
                 }
             } else if (cal_op == 3) {
