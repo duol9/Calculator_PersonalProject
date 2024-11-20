e3dec2bdb7c8e9e21986ae5538a9e801b5baa8fb
duol9 <lhyy0195@gmail.com>
2024-11-20 10:29:34 +0900

    fix : 형변환으로 인한 오류 개선
    Integer, Double 객체를 섞어서 계산할 수 있도록 개선
    - 이전엔 Integer객체를 double타입으로 다운캐스팅을 하는 과정에서 오류가 발생함
    - Integer와 Double 객체 매개변수를 Number객체로 업캐스팅 후, double타입으로 다운캐스팅하여 개선

--- src/Calculator_Lv3/ArithmeticCalculator.java
+++ src/Calculator_Lv3/ArithmeticCalculator.java
@@ -47,20 +47,31 @@ public class ArithmeticCalculator <T>  {
     // 덧셈 연산
     private T sum (T addNum1, T addNum2) { // 확인되지 않는 예외는 thorws 키워드를 사용하지 않아도 됨
         T result;
-
-        if (addNum1 instanceof Double || addNum2 instanceof Double) {  // instanceof ; addNum1이 Double 타입인지 확인 후 (true / false)로 반환 (=클래스 거짓판별기)
-            return (T) Double.valueOf((double)addNum1 + (double)addNum2); // 객체 addNum1, addNum2를 double타입으로 언박싱 후, 덧셈 연산을 하고 Double 객체 타입으로 박싱함. -> 다시 제네릭 타입으로
+        if (addNum1 instanceof Double || addNum2 instanceof Double) { // instanceof ; addNum1이 Double 타입인지 확인 후 (true / false)로 반환 (=클래스 거짓판별기)
+            if (addNum1 instanceof Double && addNum2 instanceof Double) { // 둘 다 Double 객체일 때
+                return (T) Double.valueOf((double)addNum1 + (double)addNum2); // 객체 addNum1, addNum2를 double타입으로 언박싱 후, 덧셈 연산을 하고 Double 객체 타입으로 박싱함. -> 다시 제네릭 타입으로
+            } else { // 둘 중 하나만 Double 객체일 때
+                Number num1 = (Number)addNum1;
+                Number num2 = (Number)addNum2;
+                return (T) Double.valueOf(num1.doubleValue() + num2.doubleValue() );
+            }
         } else if (addNum1 instanceof Integer && addNum2 instanceof Integer) {
             return  (T) Integer.valueOf((int)addNum1 + (int)addNum2);
-        } else {
+        }  else {
             // IllegalArgumentException : 지정한 타입이 아닌 다른 타입이 들어오면 예외 처리 함
             throw new IllegalArgumentException("이 계산기는 Integer, Double 타입만 지원합니다.");
         }
     }
     // 뺄셈연산
     private T sub (T subNum1, T subNum2) {
-        if (subNum1 instanceof Double && subNum2 instanceof Double) {
-            return (T) Double.valueOf((double)subNum1 - (double)subNum2);
+        if (subNum1 instanceof Double || subNum2 instanceof Double) {
+            if (subNum1 instanceof Double && subNum2 instanceof Double) {
+                return (T) Double.valueOf((double)subNum1 - (double)subNum2);
+            } else {
+                Number num1 = (Number)subNum1;
+                Number num2 = (Number)subNum2;
+                return (T) Double.valueOf(num1.doubleValue() - num2.doubleValue() );
+            }
         } else if (subNum1 instanceof Integer && subNum2 instanceof Integer) {
             return (T) Integer.valueOf((int)subNum1 - (int)subNum2);
         } else {
@@ -69,7 +80,12 @@ public class ArithmeticCalculator <T>  {
     }
     // 곱셈 연산
     private T mul (T mulNum1, T mulNum2) {
-        if (mulNum1 instanceof Double && mulNum2 instanceof Double) {
+        if (mulNum1 instanceof Double || mulNum2 instanceof Double) {
+            if (mulNum1 instanceof Double && mulNum2 instanceof Double) {
+                Number num1 = (Number)mulNum1;
+                Number num2 = (Number)mulNum2;
+                return (T) Double.valueOf(num1.doubleValue() * num2.doubleValue() );
+            }
             return (T) Double.valueOf((double)mulNum1 * (double)mulNum2);
         } else if (mulNum1 instanceof Integer && mulNum2 instanceof Integer) {
             return (T) Integer.valueOf((int)mulNum1 + (int)mulNum2);
@@ -81,13 +97,26 @@ public class ArithmeticCalculator <T>  {
     // 나눗셈 연산
     // 니눗셈 연산은 1. 분모에 0이 오는지 확인 . 2. 지원하지 않는 피연산자 타입 2가지의 예외 사항을 처리 해야 함
     private T div (T divNum1, T divNum2) {
-        if (divNum1 instanceof Double && divNum2 instanceof Double) {
-            if ((double)divNum2 == 0) {
-                throw new ArithmeticException("분모에 0이 올 수 없습니다.");   // 분모에 해당하는 피연산자의 값이 0이면 ArithmeticException으로 예외처리
+        if (divNum1 instanceof Double || divNum2 instanceof Double) {
+            if (divNum1 instanceof Double && divNum2 instanceof Double) {
+                if ((double)divNum2 == 0) {
+                    throw new ArithmeticException("분모에 0이 올 수 없습니다.");   // 분모에 해당하는 피연산자의 값이 0이면 ArithmeticException으로 예외처리
+                }
+                return (T) Double.valueOf((double)divNum1 * (double)divNum2);
+            } else {
+                Number num1 = (Number)divNum1;
+                Number num2 = (Number)divNum2;
+                if ((double)num2 == 0) {
+                    throw new ArithmeticException("분모에 0이 올 수 없습니다.");   // 분모에 해당하는 피연산자의 값이 0이면 ArithmeticException으로 예외처리
+                }
+                return (T) Double.valueOf(num1.doubleValue() / num2.doubleValue() );
             }
-            return (T) Double.valueOf((double)divNum1 * (double)divNum2);
         } else if (divNum1 instanceof Integer && divNum2 instanceof Integer) {
             return (T) Integer.valueOf((int)divNum1 + (int)divNum2);
+        } else if (divNum1 instanceof Double || divNum2 instanceof Double) { // 둘 중 하나만 Double 객체일 때
+            Number num1 = (Number)divNum1;
+            Number num2 = (Number)divNum2;
+            return (T) Double.valueOf(num1.doubleValue() / num2.doubleValue() );
         } else {
             throw new IllegalArgumentException("이 계산기는 Integer, Double 타입만 지원합니다.");
         }
